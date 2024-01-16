047184136c999e06218b8d2e2a573ec0ac617d0a
Moein Hosseini <moein.hoseini92@gmail.com>
2024-01-16 20:07:55 +0330

    add web view

--- lib/widgets/webview/webView.dart
+++ lib/widgets/webview/webView.dart
@@ -9,14 +9,26 @@ class WebView extends StatefulWidget {
 }
 
 class _WebViewtate extends State<WebView> {
-  Future<void> _onShowUserAgent() {
-    return WebViewController().runJavaScript(
-        '<p><iframe title="YouTube video player" src="//www.youtube.com/embed/mnEmwq_rsLw" width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>');
-    // ..loadRequest(Uri.parse('https://www.youtube.com/embed/mnEmwq_rsLw/'));
-    // return webViewController.runJavaScript(
-    //   'Toaster.postMessage("User Agent: " + navigator.userAgent);',
-    // );
-  }
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://www.youtube.com/embed/mnEmwq_rsLw'));
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +36,35 @@ class _WebViewtate extends State<WebView> {
         appBar: AppBar(
           title: const Text("Flutter WebView"),
         ),
-        body: WebViewWidget(controller: _onShowUserAgent()));
+        body: WebViewWidget(controller: controller));
   }
 }
+
+// ignore_for_file: avoid_web_libraries_in_flutter
+
+// import 'package:universal_html/parsing.dart';
+
+// import 'dart:ui' as ui;
+
+// import 'package:flutter/material.dart';
+
+// class WebView extends StatelessWidget {
+//   const WebView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//       final htmlDocument = parseHtmlDocument('<html>...</html>');
+
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           SizedBox(
+//             height: MediaQuery.of(context).size.height,
+//             width: MediaQuery.of(context).size.width,
+//             child: ,
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
